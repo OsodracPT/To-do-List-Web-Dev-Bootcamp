51a2a263c0c690efb0f2f032d4dfd16310d29453
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-06-29 10:57:36 +0100

    Styling part II

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,6 +17,6 @@ $("input[type='text']").keypress(function (event) {
         var todoText = $(this).val();
         $(this).val("");
         //create new li
-        $("ul").append("<li><span>X</span>" + todoText + "</li>")
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>" + todoText + "</li>")
     }
 });
